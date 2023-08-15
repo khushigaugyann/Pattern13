dbaf6e0bdbf52163824291cd845db47342251a25
Khushi Saini <133095807+khushigaugyann@users.noreply.github.com>
2023-08-15 08:16:23 +0530

    Add files via upload

---
+++ Pattern13.java
@@ -0,0 +1,10 @@
+public class Pattern13 {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++){
+            for(int j=i;j<=6;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
